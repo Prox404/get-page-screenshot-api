808cf9115b32aa30b7e0c8db9e6e7e138513eb8b
captainprox <92608546+captainprox02@users.noreply.github.com>
2023-05-08 12:21:48 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 const express = require('express');
 const puppeteer = require('puppeteer');
+const chromium = require('chrome-aws-lambda');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send({
-        message: 'Hello World!'
-    });
+  res.send({
+    message: 'Hello World!'
+  });
 });
 
 app.get('/screenshot', async (req, res) => {
-    const url = req.query.url;
-    const browser = await puppeteer.launch({ headless: "new" });
+  const url = req.query.url;
+  // chromium.args = [...chromium.args, '--hide-scrollbars', '--disable-web-security'];
+  // chromium.defaultViewport = chromium.defaultViewport || {width: 1280, height: 800};
+  // chromium.executablePath = await chromium.executablePath || await chromium.executablePath;
+
+
+  let browser = null;
+  try {
+
+
+    browser = await puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: "new",
+      ignoreHTTPSErrors: true,
+    });
+
+  } catch (error) {
+    console.log(error);
+  }
+
+  if (browser) {
     const page = await browser.newPage();
     await page.goto(url);
     const screenshot = await page.screenshot();
@@ -20,8 +42,14 @@ app.get('/screenshot', async (req, res) => {
       'Content-Length': screenshot.length
     });
     res.end(screenshot);
-  });
+  }else{
+    res.send({
+      message: 'Error!'
+    });
+  }
+
+});
 
 const server = app.listen(7000, () => {
-    console.log(`Express running → PORT ${server.address().port}`);
+  console.log(`Express running → PORT ${server.address().port}`);
 });
